8859363eb5d2e99294202bdd8b817283ac83399d
meetu-03 <padaliyameet8978@gmail.com>
2026-08-03 20:51:56 +0530

    k

--- assignments/PYTHON for DATA SCIENCE/session-2/python syntax,keyword,variable.py
+++ assignments/PYTHON for DATA SCIENCE/session-2/python syntax,keyword,variable.py
@@ -2,12 +2,6 @@
 
 ANSWER...
 
-item_price=(250)
-delivary_fees=(30)
-coupon_discount=(20)
-
-print(item_price)
-
 """
 
 item_price=(250)
@@ -17,3 +11,49 @@ coupon_discount=(20)
 print(item_price)
 print(delivary_fees)
 print(coupon_discount)
+
+"""
+# 2.Write a Python script that uses indentation correctly tocheck if a Flipkart 
+ product's price is above 1000; if yes, print 'Eligible for free delivery',
+ else print 'Delivery charges apply'.Hint:Use an if-else block and make sure 
+ your indentation is consistent.
+
+
+ ANSWER...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+"""
+# 3 .Add both single-line and multi-line comments in your script explaining what each section does, using # for single-line and triple quotes for multi-line comments.
+
+ANSWER...
+
+
+Program: Student Information
+Purpose: This program stores and displays student details.
+Author: Meetu
+"""
+
+# Store student information
+name = "Meetu"
+age = 20
+city = "Morbi"
+
+# Display student information
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
