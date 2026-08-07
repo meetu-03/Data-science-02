09ea875ca12ffeb6b60ad663906dfdacb7924ee9
meetu-03 <padaliyameet8978@gmail.com>
2026-08-07 15:22:53 +0530

    k

---
+++ module-3-python/core-python/what is fuction in python.py
@@ -0,0 +1,56 @@
+'''
+# what is function in python ?
+
+  1. A function is a block of coded that can be used o completed any task there we used function
+  2. A function is re-usables one block to another block 
+  3. A function is created in python using **def keyword**
+  4. A function is block of code just like 
+
+     ```
+      def functionname():
+        statements
+      call a function
+
+     ```
+
+# arguments or parameter in function 
+
+  1. arguments or parameter in function pass single arguments
+  
+  ```
+  def name(fname):
+    statements
+  call a function 
+  ```
+  2. arguments or parameter in function pass multiple arguments
+
+  ```
+  def name(fnm,lnm):
+    statements
+  call a function 
+  ```
+
+# types of function 
+
+   There are two types of function 
+   1. user defined function 
+   2. pre-defined function 
+
+   ***user defined function**
+   def faiz():
+      nm="my name is faiz"
+      print(nm)
+
+  ***pre defined function**
+    
+    name='brijesh'
+    res=name.upper()
+    print(res)
+
+    
+    name='brijesh'
+    res=name.captalize()
+    print(res)
+
+
+'''
