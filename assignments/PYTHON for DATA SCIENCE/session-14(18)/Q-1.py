b2f95ed2a18bedd77c1f7896e131c553a2c8e092
meetu-03 <padaliyameet8978@gmail.com>
2026-09-02 23:22:27 +0530

    a

--- assignments/PYTHON for DATA SCIENCE/session-14(18)/Q-1.py
+++
@@ -1,9 +0,0 @@
-#Task 1: Use re.findall() to extract all valid phone numbers from a given string in the format '+91-XXXXXXXXXX' (e.g., '+91-9876543210'). Print the list of found numbers.
-
-import re
-
-text = "Call me at +91-9876543210 or +91-1234567890. Invalid numbers: 9876543210, +91-123."
-pattern = r'\+91-\d{10}'
-
-phone_numbers = re.findall(pattern, text)
-print("Found phone numbers:", phone_numbers)
